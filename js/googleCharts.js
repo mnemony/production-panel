d4327a893c4643b5dc7704aadcc1242d4efd5fc4
Maksym <maxymprz@gmail.com>
2020-01-13 12:39:29 +0100

    unoffical version

--- js/googleCharts.js
+++ js/googleCharts.js
@@ -1,11 +1,16 @@
 //generate chart for weekly production income share
 
-let husk1 = function () {
+const husk = function (chart) {
     google.charts.load('current', {
-        'packages': ['corechart']
+        'packages': ['corechart', 'controls']
     });
-    google.charts.setOnLoadCallback(drawChart1);
+
+    google.charts.setOnLoadCallback(chart);
+
+
 }
+
+
 
 function drawChart1() {
 
@@ -42,4 +47,49 @@ function drawChart1() {
     chart.draw(data, options);
 }
 
-husk1()
+function drawChart2() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Reason', 'Waste unit'],
+        ['Overproduction', 12],
+        ['Quallity issues', 9],
+        ['Techincal issues', 4],
+        ['Other', 5]
+    ]);
+
+    var options = {
+        backgroundColor: 'transparent',
+        pieSliceText: 'percentage',
+
+        legend: {
+            position: 'top',
+            maxLines: 3,
+        },
+
+        slices: {
+            0: {
+                color: '#F08080'
+            },
+            1: {
+                color: '#daa520'
+            },
+            2: {
+                color: '#008080'
+            },
+            3: {
+                color: '#7fffd4'
+            },
+        }
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+
+    chart.draw(data, options);
+}
+
+
+
+
+
+husk(drawChart1)
+husk(drawChart2)
--- js/warehouse.js
+++ js/warehouse.js
@@ -1,12 +1,15 @@
 const allBars = document.querySelectorAll('.bar-fill');
 const redish = 'rgba(207, 47, 36, 0.801)';
 const greeny = 'rgba(139, 207, 36, 0.801)';
+const yellowlike = '#d4ca33c5';
 
 // check state of warehouses
 
 allBars.forEach(element => {
     if (parseFloat(element.style.width) >= 85) {
         element.style.backgroundColor = redish;
+    } else if (parseFloat(element.style.width) <= 25) {
+        element.style.backgroundColor = yellowlike;
     } else {
         element.style.backgroundColor = greeny;
     }
